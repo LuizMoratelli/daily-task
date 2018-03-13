ef8a9cc8da7d634ee74a95ad2f9a8287ec887877
Luiz <gameplayteam12@gmail.com>
2018-03-13 01:19:33 -0300

    Aula 1

---
+++ client/taskList/taskList.js
@@ -0,0 +1,11 @@
+Template.taskList.helpers({
+
+
+    tasks: function() {
+        return [
+            {nome: "Ir ao mercado"},
+            {nome: "Estudar"},
+            {nome: "Nunca desistir"}    
+        ];
+    }
+});
